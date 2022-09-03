66aab3acc1e42f1d893705481311640d95af5c81
Ronny <ronnybet41@gmail.com>
2022-09-03 17:23:08 +0300

    add scripts

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,34 +1,98 @@
-let btn1;
-let btn2;
-let btn3;
-let btn4;
 
-let p1 = document.getElementById('p1')
-let p2 = document.createElement('p2')
-let p3 = document.createElement('p3')
-let p4 = document.createElement('p4')
-let p5 = document.getElementById('p5')
+const container= document.querySelector(".section")
 
+const renderDetails = async () => {
 
+  let url = "http://localhost:3000/personDetails"
 
+  const res = await fetch(url);
+  const personDetails = await res.json();
+  //console.log(personDetails)   
 
-function onLoad() {
-  fetch("http://localhost:3000/person/1")
-    .then((resp) => resp.json())
-    .then((data) => {
-      displayImage(data);
-    });
+  let template = '';
+  personDetails.forEach(details => {
+    template += `
+<div id="sec-A">
+            <img src=${details.image} alt="image">
+            <h3>${details.name}</h3>
+            <p>${details.idNumber}</p>
+            <p> ${details.mobile}</p>
+            <p> ${details.pollingStation}</p>
+            <p >${details.addres}</p>
+            <p>${details.status}</p>
 
+        </div> 
+
+`
+
+  });
+  container.innerHTML = template; 
 }
-function displayImage() {
-  let div = document.getElementById('sec-A');
-  div.innerHTML = data.values
-  image.src = data.image
-  p1.textContent = data.name
-  p1.textContent = data.mobile
-}
+window.addEventListener('DOMContentLoaded', () => renderDetails());
 
-// document.addEventListener('DOMContentLoaded', onLoad)
-// const personImage = document.getElementById('image')
-// const name = document.getElementById('name')
-displayImage()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let btn1;
+// let btn2;
+// let btn3;
+// let btn4;
+
+// let p1 = document.getElementById('p1')
+// let p2 = document.createElement('p2')
+// let p3 = document.createElement('p3')
+// let p4 = document.createElement('p4')
+// let p5 = document.getElementById('p5')
+
+
+
+
+// function onLoad() {
+//   fetch("http://localhost:3000/person/1")
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//       personDetails(data);
+
+
+//       console.log(data)
+//     });
+
+
+// function personDetails() {
+//   let div = document.getElementById('sec-A');
+//   div.innerHTML = data.values
+//   image.src = data.image
+//   p1.textContent = data.name
+//   p1.textContent = data.mobile
+// }
+// p1.append
+// // document.addEventListener('DOMContentLoaded', onLoad)
+// // const personImage = document.getElementById('image')
+// // const name = document.getElementById('name')
+// personDetails()
